bb6037160f0f083857b8e53eb8402569e99b3be6
svenzhi <svenzhi@163.com>
2025-11-07 23:07:03 +0800

    新增流量统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -13,6 +13,17 @@ export default defineUserConfig({
 
   theme,
 
+  head: [
+    [
+      'script',
+      {
+        defer: '',
+        src: 'https://u.itsven.cn/my-analytics.js',
+        'data-website-id': '719acfab-7aa1-42a8-bceb-b2ccd27628b1',
+      },
+    ],
+  ],
+
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
